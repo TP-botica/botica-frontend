67c8a298a4fa7859a7f1c9594353292b70870d96
Angel Sosa <gabrielo.09.angel@gmail.com>
2024-06-08 16:41:02 -0500

    initial login

--- src/app/modules/presentation/components/login/login.component.ts
+++ src/app/modules/presentation/components/login/login.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
--- src/app/modules/presentation/presentation.module.ts
+++ src/app/modules/presentation/presentation.module.ts
@@ -5,14 +5,17 @@ import { PresentationRoutingModule } from './presentation-routing.module';
 import { CarouselModule } from 'primeng/carousel';
 import { ButtonModule } from 'primeng/button';
 import { TagModule } from 'primeng/tag';
+import { InputTextModule } from 'primeng/inputtext';
+
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, LoginComponent],
   imports: [
     CommonModule,
     PresentationRoutingModule,
-    CarouselModule, ButtonModule, TagModule
+    CarouselModule, ButtonModule, TagModule, InputTextModule
   ]
 })
 export class PresentationModule { }
