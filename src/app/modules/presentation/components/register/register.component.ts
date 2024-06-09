c2b756a8e3589bf1e2e23b1c0b92ad1aeea91de3
Angel Sosa <gabrielo.09.angel@gmail.com>
2024-06-09 14:57:23 -0500

    basic screens done

--- src/app/modules/presentation/components/register/register.component.ts
+++ src/app/modules/presentation/components/register/register.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Role } from '../../../../domain/role';
 
 @Component({
   selector: 'app-register',
-  standalone: true,
-  imports: [],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  roles: Role[] | undefined;
 
+  selectedRole: Role | undefined;
+
+  ngOnInit() {
+      this.roles = [
+          { name: 'Cliente', id: 'C' },
+          { name: 'Botica', id: 'B' },
+      ];
+  }
+
+  register(){
+    console.log(this.selectedRole)
+  }
 }
--- src/app/modules/presentation/presentation.module.ts
+++ src/app/modules/presentation/presentation.module.ts
@@ -6,16 +6,20 @@ import { CarouselModule } from 'primeng/carousel';
 import { ButtonModule } from 'primeng/button';
 import { TagModule } from 'primeng/tag';
 import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
+
 
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
-  declarations: [HomeComponent, LoginComponent],
+  declarations: [HomeComponent, LoginComponent,RegisterComponent],
   imports: [
     CommonModule,
     PresentationRoutingModule,
-    CarouselModule, ButtonModule, TagModule, InputTextModule
+    CarouselModule, ButtonModule, TagModule, InputTextModule,DropdownModule,FormsModule
   ]
 })
 export class PresentationModule { }
