76c5239ce1e5d4fac12e614f6d3f3f5f4c3dced5
Angel Sosa <gabrielo.09.angel@gmail.com>
2024-11-10 19:20:04 -0500

    fixing things

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,2 +1,2 @@
-//export const myUrl: string = 'https://api.medifinderperu.com';
-export const myUrl: string = 'http://localhost:8090';
+export const myUrl: string = 'https://api.medifinderperu.com';
+//export const myUrl: string = 'http://localhost:8090';
